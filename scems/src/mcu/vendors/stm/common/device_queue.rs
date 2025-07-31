84afa6ac0a2102f5b81ffab42bd56cc6f655d3cf
Khose-ie <khose-ie@outlook.com>
2025-07-31 22:14:55 +0800

    Create some comment to STM device queue.

--- scems/src/mcu/vendors/stm/common/device_queue.rs
+++ scems/src/mcu/vendors/stm/common/device_queue.rs
@@ -1,3 +1,20 @@
+//! A queue data structure to save the pointer of peripheral devices.
+//!
+//! The main function of DeviceQueue is to save the pointer of peripheral device which has been set the event agent.
+//! The reason why we need such a data structure is came from the status of STM32 HAL libraries.
+//! In STM32 HAL libraries, all same-kind peripheral will have the same callback function, and the handle pointer 
+//! will be transported as the argument of the callback function, the caller should judgement the argument, and do 
+//! the divided action with some branch logic like `if-else` statement.
+//!
+//! In scems, the event agent, an interface who provide the specification of the callback function, will be registered
+//! to every peripheral. So we need to map the single callback function with the multiple event agent, by the only 
+//! information of pointer of handle.
+//!
+//! A naturally methods is to create a map table, save the device instance and handle pointer, when the device be 
+//! registered event agent, this device will be add to the map table. And the single callback function will find 
+//! the device according to the argument of handle pointer.
+//! So, that is what the DeviceQueue is.
+
 use core::marker::PhantomData;
 use core::ptr::null_mut;
 
@@ -5,6 +22,15 @@ use crate::common::cast::CastOpt;
 use crate::common::result::{IError, IResult};
 use crate::mcu::common::HandlePtr;
 
+/// A queue template to be used to map the peripheral device and the handler pointer of it.
+/// 
+/// To instantiate this template, you need provide 3 types or information.
+/// 
+/// 1. T, the type of the handle of the peripheral device.
+///    Usually, we use handle with pointer, but the T is not a pointer type, you should provide the data type of it, 
+/// 2. U, the type of the peripheral device.
+/// 3. N, the count or the max length of the queue. It always be decided by the count of the peripheral device of the 
+///    MCU you choice.
 pub struct DeviceQueue<T, U, const N: usize>
 where
     U: HandlePtr<T>,
