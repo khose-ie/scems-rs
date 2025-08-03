658a45a872a7de6179ef77a3fc5bb6e23f74ff8a
Khose-ie <khose-ie@outlook.com>
2025-08-03 21:54:32 +0800

    Create an example to echo the message from UART.

--- main/src/example.rs
+++ main/src/example.rs
@@ -1,8 +1,13 @@
-mod io_lamp;
+use scems::os::{common::task::TaskMain, vendors::task::TaskSample};
 
-pub fn example()
+mod io_lamp;
+mod uart_echo;
+
+pub unsafe fn example()
 {
     #[cfg(feature = "example-io-lamp")]
     io_lamp::main();
 
+    #[cfg(feature = "example-uart-echo")]
+    uart_echo::main();
 }
---
+++ main/src/example/uart_echo.rs
@@ -0,0 +1,43 @@
+use scems::mcu::common::uart::UartDevice;
+use scems::mcu::vendor::stm::uart::{UART_HandleTypeDef, UartQueue};
+
+#[allow(improper_ctypes)]
+extern "C" {
+    static mut huart1: UART_HandleTypeDef;
+}
+
+#[allow(static_mut_refs)]
+pub unsafe fn main()
+{
+    let mut example = UartEchoExample::new(UartQueue::allocate(&mut huart1).unwrap());
+
+    loop
+    {
+        example.tick();
+    }
+}
+
+pub struct UartEchoExample
+{
+    uart: UartDevice,
+    cache: [u8; 1024],
+}
+
+impl UartEchoExample
+{
+    pub fn new(uart: UartDevice) -> Self
+    {
+        UartEchoExample { uart, cache: [0; 1024] }
+    }
+
+    pub fn tick(&mut self)
+    {
+        let value = self.uart.receive(&mut self.cache, 1000);
+
+        if let Ok(x) = value
+        {
+            #[allow(unused_must_use)]
+            self.uart.transmit(&self.cache[0..x as usize], 1000);
+        }
+    }
+}
